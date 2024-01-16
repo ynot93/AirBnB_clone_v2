054967cdcc7025897199854c218da7d0359b2056
Tony Mputhia <tonymmputhia@gmail.com>
2024-01-16 01:19:07 -0800

    Implement DBStorage with SQLAlchemy

--- models/city.py
+++ models/city.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel
+#from sqlalchemy import Column, String, ForeignKey
+#from sqlalchemy.orm import relationship
+#from sqlalchemy.ext.declarative import declarative_base
+
+#Base = declarative_base()
 
 
 class City(BaseModel):
