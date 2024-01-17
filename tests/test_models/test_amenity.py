4ee3543c24bfe21f973d06a6cdb262caaa328f8d
millyanne93 <nmillyanne20@gmail.com>
2024-01-17 09:56:13 -0800

    unittests for amenity and basemodel

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,19 +1,54 @@
 #!/usr/bin/python3
-""" """
+"""Amenity test """
 from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
+import os
+import unittest
 
 
-class test_Amenity(test_basemodel):
-    """ """
+class TestAmenity(test_basemodel):
+    """Amenity test class"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """Init the test class"""
         super().__init__(*args, **kwargs)
         self.name = "Amenity"
         self.value = Amenity
 
     def test_name2(self):
-        """ """
+        """Testing name type"""
         new = self.value()
-        self.assertEqual(type(new.name), str)
+        self.assertEqual(type(new.name), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
+
+    def test_inheritance(self):
+        """Test that Amenity inherits from BaseModel"""
+        amenity = self.value()
+        self.assertIsInstance(amenity, BaseModel)
+
+    def test_attributes(self):
+        """Test that Amenity has the expected attributes"""
+        amenity = self.value()
+        self.assertTrue(hasattr(amenity, 'name'))
+
+    def test_attributes_type(self):
+        """Test that Amenity attributes have the correct types"""
+        amenity = self.value()
+        self.assertIsInstance(amenity.name, str)
+
+    def test_attributes_default_value(self):
+        """Test that Amenity attributes have the correct default values"""
+        amenity = self.value()
+        self.assertEqual(amenity.name, "")
+
+@unittest.skipIf(getenv('HBNB_TYPE_STORAGE') != 'db', 'not testing file storage')
+class TestAmenityFileStorage(unittest.TestCase):
+    def test_relationship(self):
+        """Test that Amenity has the correct relationship with Place in file storage"""
+        amenity = Amenity()
+        self.assertTrue(hasattr(amenity, 'place_amenities'))
+        self.assertIsInstance(amenity.place_amenities, list)
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" """
+"""Basemodel unittest """
 from models.base_model import BaseModel
 import unittest
 import datetime
@@ -7,40 +7,42 @@ from uuid import UUID
 import json
 import os
 
-
+@unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') == 'db',
+                 'basemodel test not supported')
 class test_basemodel(unittest.TestCase):
-    """ """
+    """Test class for the base_model class"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """Init the test class of base_model class """
         super().__init__(*args, **kwargs)
         self.name = 'BaseModel'
         self.value = BaseModel
 
     def setUp(self):
-        """ """
+        """Set up method of test class"""
         pass
 
     def tearDown(self):
+        """Teardown method of test class"""
         try:
             os.remove('file.json')
         except:
             pass
 
     def test_default(self):
-        """ """
+        """default testing of the basemode"""
         i = self.value()
         self.assertEqual(type(i), self.value)
 
     def test_kwargs(self):
-        """ """
+        """Test basemodel with kwargs """
         i = self.value()
         copy = i.to_dict()
         new = BaseModel(**copy)
         self.assertFalse(new is i)
 
     def test_kwargs_int(self):
-        """ """
+        """Testing kwargs with int kwargs """
         i = self.value()
         copy = i.to_dict()
         copy.update({1: 2})
@@ -57,41 +59,53 @@ class test_basemodel(unittest.TestCase):
             self.assertEqual(j[key], i.to_dict())
 
     def test_str(self):
-        """ """
+        """Testing the str method """
         i = self.value()
         self.assertEqual(str(i), '[{}] ({}) {}'.format(self.name, i.id,
                          i.__dict__))
 
     def test_todict(self):
-        """ """
-        i = self.value()
-        n = i.to_dict()
-        self.assertEqual(i.to_dict(), n)
+        """Testing to_dict method"""
+        my_model = BaseModel()
+        my_model.name = "Holberton"
+        my_model.my_number = 89
+        d = my_model.to_dict()
+        expected_attrs = ["id",
+                          "created_at",
+                          "updated_at",
+                          "name",
+                          "my_number",
+                          "__class__"]
+        self.assertCountEqual(d.keys(), expected_attrs)
+        self.assertEqual(d['__class__'], 'BaseModel')
+        self.assertEqual(d['name'], "Holberton")
+        self.assertEqual(d['my_number'], 89)
+        self.assertNotIn('_sa_instance_state', d)
 
     def test_kwargs_none(self):
-        """ """
+        """Testing kwargs with none"""
         n = {None: None}
         with self.assertRaises(TypeError):
             new = self.value(**n)
 
     def test_kwargs_one(self):
-        """ """
+        """testing kwargs with one argument"""
         n = {'Name': 'test'}
-        with self.assertRaises(KeyError):
-            new = self.value(**n)
+        new = self.value(**n)
+        self.assertEqual(new.name, n['name'])
 
     def test_id(self):
-        """ """
+        """Test id attr"""
         new = self.value()
         self.assertEqual(type(new.id), str)
 
     def test_created_at(self):
-        """ """
+        """Testing created at"""
         new = self.value()
         self.assertEqual(type(new.created_at), datetime.datetime)
 
     def test_updated_at(self):
-        """ """
+        """Test updated at"""
         new = self.value()
         self.assertEqual(type(new.updated_at), datetime.datetime)
         n = new.to_dict()
