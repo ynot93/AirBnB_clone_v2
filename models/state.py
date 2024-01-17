27a36257045a43e2a68b519b5e394ba41c8c932d
Tony Mputhia <tonymmputhia@gmail.com>
2024-01-16 20:10:11 -0800

    Add ORM to city and state classes

--- models/state.py
+++ models/state.py
@@ -1,8 +1,35 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.city import City
+import os
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+
+    name= Column(String(128), nullable=False)
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship(
+                "City", 
+                backref="state",
+                cascade="all, delete-orphan")
+
+    else:
+        @property
+        def cities(self):
+            """
+            Returns City objects with state_id equal to
+            State.id
+
+            """
+            from models import storage
+            city_list = []
+            for city_id, city in storage.all(City):
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_lost
