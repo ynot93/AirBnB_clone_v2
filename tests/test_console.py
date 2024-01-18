9cdc0c60c795dca89d0773f484e02786f2db96ee
millyanne93 <nmillyanne20@gmail.com>
2024-01-18 03:44:37 -0800

    fix clear stream error

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """A unit test module for the console (command interpreter).
 """
+"""
+test module
+"""
 import json
 import MySQLdb
 import os
@@ -13,7 +16,6 @@ from console import HBNBCommand
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
-from tests import clear_stream
 
 
 class TestHBNBCommand(unittest.TestCase):
