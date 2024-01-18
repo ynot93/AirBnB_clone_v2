29a2786bbaeaba6b85b4246dff890e8c941150d0
millyanne93 <nmillyanne20@gmail.com>
2024-01-18 00:07:10 -0800

    fix pep8 errors

--- models/amenity.py
+++ models/amenity.py
@@ -19,8 +19,10 @@ class Amenity(BaseModel, Base):
         place_amenities = Table(
                 'place_amenity',
                 Base.metadata,
-                Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
-                Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False)
+                Column('place_id', String(60), ForeignKey('places.id'),
+                        primary_key=True, nullable=False),
+                Column('amenity_id', String(60), ForeignKey('amenities.id'),
+                       primary_key=True, nullable=False)
         )
 
         places = relationship(
