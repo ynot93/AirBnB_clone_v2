46e12874ae51f199d5282cb711cc9aaa53565087
Tony Mputhia <tonymmputhia@gmail.com>
2024-02-01 17:28:15 -0800

    Prepare both web servers

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# Sets up web servers for the deployment of web static
+
+# Install Nginx if not already installed
+sudo apt-get update
+sudo apt-get -y install nginx
+
+# Create necessary folders
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+sudo chown -R ubuntu:ubuntu /data
+
+# Create test HTML file
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+  </html>" | sudo tee /data/web_static/releases/test/index.html
+
+# Recreate symbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# Update Nginx configuration
+nginx_config="/etc/nginx/sites-available/default"
+nginx_alias="\tlocation /hbnb_static/ {\n\talias /data/web_static/current/;\n}\n"
+sudo sed -i "37i $nginx_alias" "$nginx_config"
+
+# Restart Nginx
+sudo service nginx restart
