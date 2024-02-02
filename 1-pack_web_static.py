d95d297463ffc341ba59aae205850d80e46829f3
Tony Mputhia <tonymmputhia@gmail.com>
2024-02-01 17:52:45 -0800

    Generate .tgz archive

---
+++ 1-pack_web_static.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Generates a .tgz archive from web_static folder
+
+"""
+
+from fabric.api import local
+from datetime import datetime
+import os
+
+def do_pack():
+    """
+    Packs web_static into a .tgz archive.
+
+    Returns:
+        (str): Path of archive if successful, otherwise, none.
+    """
+    if not os.path.exists("versions"):
+        local("mkdir -p versions")
+
+    timestamp = datetime.utcnow().strftime("%Y%m%d%H%M%S")
+    archive_name = "web_static_{}.tgz".format(timestamp)
+    archive_path = "versions/{}".format(archive_name)
+
+    result = local("tar -cvzf {} web_static".format(archive_path))
+
+    if result.succeeded:
+        return archive_path
+    else:
+        return None
+
+if __name__ == "__main__":
+    do_pack()
