a654ab20f7dd7a2299a7eea47655af1c8e9e2eb0
Tony Mputhia <tonymmputhia@gmail.com>
2024-02-15 14:53:18 +0000

    C is fun!

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web app with host as 0.0.0.0 and
+on port 5000.
+
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """
+    Display Hello HBNB!
+
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello():
+    """
+    Display HBNB
+
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """
+    Display dynamic variable on route
+
+    """
+    text = text.replace('_', ' ')	
+    return f"C {text}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
