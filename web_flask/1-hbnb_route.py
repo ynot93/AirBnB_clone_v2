f37d6da137db95b9890514c808735dedc2385bf1
Tony Mputhia <tonymmputhia@gmail.com>
2024-02-15 14:44:29 +0000

    Display HBNB on route /hbnb

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web app with host as 0.0.0.0 and
+on port 5000.
+
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """
+    Display Hello HBNB!
+
+    """
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hello():
+    """
+    Display Hello HBNB!
+
+    """
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
