d6d3f867539c61ad91362a593fab7e1386d446e5
Tony Mputhia <tonymmputhia@gmail.com>
2024-02-16 11:52:52 +0000

    Add flask template if n is even or odd

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web app with host as 0.0.0.0 and
+on port 5000.
+
+"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """
+    Display Hello HBNB!
+
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Display HBNB
+
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """
+    Display dynamic variable on route
+
+    """
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/(<text>)', strict_slashes=False)
+def python(text='is_cool'):
+    """
+    Display dynamic variable on route /python/
+
+    """
+    text = text.replace('_', ' ')
+    return "Python {}".format(text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """
+    Display dynamic variable on route /number/
+
+    """
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """
+    Display dynamic variable on route /number_template/
+
+    """
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    """
+    Display dynamic variable on route /number__odd_or_even/
+
+    """
+    odd_even = ('even' if n % 2 == 0 else 'odd')
+    return render_template('6-number_odd_or_even.html', n=n, odd_even=odd_even)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
