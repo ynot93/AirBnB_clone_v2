d743e45c4eef2d7fb28b729fe1e03379ef24ff47
Tony Mputhia <tonymmputhia@gmail.com>
2024-02-19 05:40:52 +0300

    States and State

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,45 +1,46 @@
 #!/usr/bin/python3
 """
-Displays a list of all states.
+Script to start a Flask web application"""
 
-"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
 
-
 app = Flask(__name__)
 
-@app.teardown_appcontext
-def clean_up(exception=None):
-    """
-    Performs clean up tasks e.g closing DB connection.
-
-    """
-    storage.close()
 
 @app.route('/states', strict_slashes=False)
-def list_states():
+def states():
     """
-    Lists all state objects.
+    Display a list of states
 
     """
     states = storage.all(State).values()
     return render_template('9-states.html', states=states)
 
 
-@app.route('/states/<state_id>', strict_slashes=False)
-def list_cities(id):
+@app.route('/states/<id>', strict_slashes=False)
+def state_cities(id):
     """
-    Lists cities of a given state.
+    Display cities associated with a state
 
     """
-    states = storage.all(State).values()
-    for state in states:
-        if state.id == id:
-            return render_template('9-states.html', state=state)
-    return render_template('9-not_found.html', states=states)
+    state = storage.get(State, id)
+    if state:
+        return render_template('9-states.html', states=[state])
+    else:
+        return render_template('9-states.html', states=None)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """
+    Closes the database connection after each request
+
+    """
+    storage.close()
 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
+
